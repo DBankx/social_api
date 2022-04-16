db8d28f46fcf6e6e69f7b4f40aabf545c865413c
Hundeyin Oluwadamilola <d.hundeyin@yahoo.com>
2022-04-16 12:51:58 -0400

    worked on basic signup endpoint and added hashing

--- src/api/auth/effects/signup.effect.ts
+++ src/api/auth/effects/signup.effect.ts
@@ -1,10 +1,12 @@
-import { r, RouteEffect } from '@marblejs/http';
-import { map, mergeMap } from 'rxjs/operators';
+import { r, RouteEffect, HttpError, HttpStatus } from '@marblejs/http';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
 import { TUserDto } from '../dto/signup.dto';
 import { AuthDao } from '../helpers/auth.dao';
 import signupValidator from '../validators/signup.validator';
 import makeApiResponse from '../../../utils/apiResponse.utils';
 import bcrypt from 'bcryptjs';
+import { User } from '@prisma/client';
+import { throwError } from 'rxjs';
 
 const signup$: RouteEffect = r.pipe(
 	r.matchPath('/signup'),
@@ -13,9 +15,16 @@ const signup$: RouteEffect = r.pipe(
 		signupValidator,
 		map(req => req.body),
 		mergeMap((user: any) => {
-			const salt = bcrypt.genSaltSync(10);
-			user.password = bcrypt.hashSync(user.password, salt);
-			return AuthDao.createUser(user);
+			return AuthDao.findUserByEmail(user.email).pipe(
+				switchMap((foundUser) => {
+					if(foundUser){
+						return throwError(new HttpError('User already exists', HttpStatus.BAD_REQUEST));
+					} 
+					const salt = bcrypt.genSaltSync(10);
+					user.password = bcrypt.hashSync(user.password, salt);
+					return AuthDao.createUser(user);
+				})
+			)
 		}),
 		map(body => ({body: makeApiResponse('User created successfully', null, true)})),
 	))
--- src/api/auth/helpers/auth.dao.ts
+++ src/api/auth/helpers/auth.dao.ts
@@ -5,6 +5,13 @@ import { TUserDto } from '../dto/signup.dto';
 const prisma = new PrismaClient();
 
 export namespace AuthDao {
+	export const findUserByEmail = (email: string) => from (
+		prisma.user.findUnique({
+			where: {
+				email
+			}
+		})
+	)
 	export const createUser = (user: TUserDto) => from(
 		prisma.user.create({
 			data: user
